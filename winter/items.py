17f5a4434dc0e178b7a08073d1e255d64ba9c695
freeyiyi1993 <freeyiyi1993@gmail.com>
2015-05-31 19:08:09 +0800

    items

--- winter/items.py
+++ winter/items.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
+from scrapy.item import Item, Field
 
-import scrapy
+class QuestionItem(scrapy.Item):
+  question = Field()
+  url = Field()
 
-
-class WinterItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class AnswerItem(scrapy.Item):
+	url = Field()
+	question_title = Field()	
+	desc = Field()
+	answer = Field()
